34c4c531ea53eff53bdccb351c072dd28551ab4e
VVhatIsLove <sam4uk.v1ad@gmail.com>
2023-02-24 13:14:11 +0200

    Refactor code

--- src/components/table/balancesTable/Links.tsx
+++ src/components/table/balancesTable/Links.tsx
@@ -1,99 +1,14 @@
 import { LinkWithIcon } from '../utils'
-import { resolveSubscanUrl, linksByNetworks, NetworkLinks } from '../links'
+import { linksByNetworks, NetworkLinks } from '../links'
 import { Menu, Dropdown, Button, Divider } from 'antd'
 import { useCurrentAccount } from '../../providers/MyExtensionAccountsContext'
-import { MoreOutlined, GlobalOutlined, TwitterOutlined, GithubOutlined } from '@ant-design/icons'
+import { MoreOutlined } from '@ant-design/icons'
 import styles from './Links.module.sass'
-import { SubIcon } from '../../utils/index'
-import { RiDiscordLine } from 'react-icons/ri' 
-import { FaTelegramPlane } from 'react-icons/fa'
-import { AiFillAppstore } from 'react-icons/ai'
-import { FiSend } from 'react-icons/fi'
 import { isDef } from '@subsocial/utils'
 import clsx from 'clsx'
 import { useState } from 'react'
 import { useResponsiveSize } from '../../responsive/ResponsiveContext'
-
-type MenuItemsLinkWithLogoProps = {
-  link: string
-  label: string
-  icon: React.ReactNode
-}
-
-const MenuItemsLinkWithLogo = ({ link, label, icon }: MenuItemsLinkWithLogoProps) => {
-  return <LinkWithIcon link={link} label={label} icon={icon} />
-}
-
-type SubscanMenuItemLinkProps = {
-  network: string
-  address?: string
-}
-
-const SubscanMenuItemLink = ({ network, address }: SubscanMenuItemLinkProps) => {
-  if(!address) return <></>
-
-  const link = resolveSubscanUrl(network, address)
-
-  if (!link) return <></>
-
-  return <MenuItemsLinkWithLogo link={link} label={'Subscan'} icon='/images/subscan.svg' />
-}
-
-const actionMenuItemsValues: Record<string, LinkIconsAndLabelsValue> = {
-  subscanSubdomain: {
-    icon: <GlobalOutlined />,
-    label: 'Website'
-  },
-  actionTransfer: {
-    icon: <SubIcon Icon={FiSend} className={styles.TransferIcon}/>,
-    label: 'Transfer Tokens',
-  }
-}
-
-type LinkIconsAndLabelsValue = {
-  icon: React.ReactNode
-  label: string
-}
-
-const socialMenuItemsValues: Record<string, LinkIconsAndLabelsValue> = {
-  website: {
-    icon: <GlobalOutlined />,
-    label: 'Website'
-  },
-  twitter: {
-    icon: <TwitterOutlined />,
-    label: 'Twitter'
-  },
-  discord: {
-    icon: <SubIcon Icon={RiDiscordLine} />,
-    label: 'Discord'
-  },
-  telegram: {
-    icon: <SubIcon Icon={FaTelegramPlane} />,
-    label: 'Telegram'
-  },
-  github: {
-    icon: <GithubOutlined />,
-    label: 'Github'
-  },
-  app: {
-    icon: <SubIcon Icon={AiFillAppstore} />,
-    label: 'DApp'
-  }
-}
-
-type ActionButtonProps = {
-  label: string
-  icon: React.ReactNode
-  action?: (e: any) => void
-}
-
-const ActionButton = ({ label, icon, action }: ActionButtonProps) => {
-  return <div onClick={action} className='d-flex align-items-center'>
-    <div className={clsx(styles.ActionIconWrapp, 'LinkWithIcon')}>{icon}</div>
-    <div className='ml-3'>{label}</div>
-  </div>
-}
+import { socialMenuItemsValues, actionMenuItemsValues, SubscanMenuItemLink, ActionButton } from './utils'
 
 type LinkButtonsOverlayProps = {
   network: string
