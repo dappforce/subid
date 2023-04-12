a3228369532820ac00e31bf09561921c6536f3f9
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-04-13 00:40:41 +0700

    Add sorting based on balances for chats

--- src/components/chat/ChatFloatingModal.tsx
+++ src/components/chat/ChatFloatingModal.tsx
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useAppDispatch, useAppSelector } from 'src/rtk/app/store'
 import { BsFillChatLeftTextFill } from 'react-icons/bs'
 import styles from './ChatFloatingModal.module.sass'
 import { chatActions } from 'src/rtk/features/chat/chatSlice'
+import { useSortMyBalances } from 'src/utils/hooks/useSortMyBalances'
 import { Button } from 'antd'
+import { TOKEN_TO_CHAT_ID } from './chat-ids'
 
 export default function ChatFloatingModal () {
-  const { isOpen, chatId } = useAppSelector((state) => state.chat)
+  const { isOpen } = useAppSelector((state) => state.chat)
   const dispatch = useAppDispatch()
-  const openChat = () => dispatch(chatActions.openChat())
+  const toggleChat = () => dispatch(chatActions.toggleChat())
 
-  if (!chatId) return null
+  const balances = useSortMyBalances()
+  const order = useMemo<string[]>(() => {
+    if (balances.length === 0) return []
+    const appendedToken = new Set()
+    return balances
+      .map(({ token }) => {
+        if (appendedToken.has(token)) return ''
+        const tokenId = TOKEN_TO_CHAT_ID?.[token.toLowerCase() as keyof typeof TOKEN_TO_CHAT_ID]
+        if (tokenId) appendedToken.add(token)
+        return tokenId
+      })
+      .filter((id) => !!id)
+  }, [ balances ])
+
+  const urlParam = new URLSearchParams()
+  order.forEach((id) => {
+    urlParam.append('order', id)
+  })
+
+  if (balances.length === 0) return null
+
   return (
     <div className={styles.ChatFloatingModal}>
       {isOpen && (
         <div className={styles.ChatFloatingIframe}>
-          <iframe src={`https://x.grill.app/c/${chatId}`} />
+          <iframe src={`https://iframe-configs-grillchat.subsocial.network/1004?${urlParam.toString()}`} />
         </div>
       )}
-      <Button className={styles.ChatFloatingButton} onClick={openChat}>
+      <Button className={styles.ChatFloatingButton} onClick={toggleChat}>
         <BsFillChatLeftTextFill />
       </Button>
     </div>
---
+++ src/components/chat/chat-ids.ts
@@ -0,0 +1,7 @@
+export const TOKEN_TO_CHAT_ID = {
+  astr: '1610',
+  kma: '1611',
+  cfg: '1612',
+  kilt: '1613',
+  sdn: '1614',
+}
