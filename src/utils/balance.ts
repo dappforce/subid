1876bcfa1567857c3e5c4bcda8f1c425e16fa850
Sam <sam4uk.v1ad@gmail.com>
2023-10-23 18:42:36 +0300

    Fix bug

--- src/utils/balance.ts
+++ src/utils/balance.ts
@@ -2,8 +2,10 @@ import BN from 'bn.js'
 import { BN_ZERO } from '@polkadot/util'
 import { AccountInfoByChain } from 'src/components/identity/types'
 
-export const getTransferableBalance = (data: AccountInfoByChain | undefined) => {
+export const getTransferableBalance = (
+  data: AccountInfoByChain | undefined
+) => {
   if (!data) return BN_ZERO
-  const { freeBalance, frozenMisc, frozenFee } = data
-  return new BN(freeBalance).sub(new BN(frozenMisc || frozenFee || BN_ZERO))
+  const { freeBalance } = data
+  return new BN(freeBalance)
 }
