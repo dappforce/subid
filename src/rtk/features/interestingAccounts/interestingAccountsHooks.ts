e9606b39380ebafd193f083dd2ee64f21ce4a60d
Sam <sam4uk.v1ad@gmail.com>
2023-11-09 13:20:37 +0200

    Fetch interesting accounts on the only search page

--- src/rtk/features/interestingAccounts/interestingAccountsHooks.ts
+++ src/rtk/features/interestingAccounts/interestingAccountsHooks.ts
@@ -1,14 +1,23 @@
-import { Dispatch } from 'react'
-import { useAppSelector } from '../../app/store'
-import { selectOverviewAccounts, overviewAccountsActions } from './interestingAccountsSlice'
+import { Dispatch, useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../../app/store'
+import {
+  selectOverviewAccounts,
+  overviewAccountsActions,
+} from './interestingAccountsSlice'
 import { AnyAction } from 'redux'
 import { AccountCardType } from '../../../components/interesting-accounts/types'
 
 export const fetchOverviewAccounts = (dispatch: Dispatch<AnyAction>) =>
   dispatch(overviewAccountsActions.fetchOverviewAccounts())
 
+export const useFetchOverviewAccounts = () => {
+  const dispatch = useAppDispatch()
+  
+  useEffect(() => {
+    fetchOverviewAccounts(dispatch)
+  }, [])
+}
 export const useOverviewAccounts = () =>
   useAppSelector<AccountCardType[] | undefined>((state) =>
     selectOverviewAccounts(state)
   )
-
