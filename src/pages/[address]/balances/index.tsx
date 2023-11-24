2e99a0ca5cf744622bbbf1b520d60624c0339bbf
Sam <sam4uk.v1ad@gmail.com>
2023-11-24 12:49:44 +0200

    Change push to replace

--- src/pages/[address]/balances/index.tsx
+++ src/pages/[address]/balances/index.tsx
@@ -2,13 +2,14 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 const BalancesPage = () => {
-  const { query, push } = useRouter()
+  const { query, replace } = useRouter()
 
   const address = query.address as string
 
   useEffect(() => {
     if (!address) return
-    push({ pathname: '/[address]', query: { ...query, tab: 'portfolio' } })
+    
+    replace({ pathname: '/[address]', query: { ...query, tab: 'portfolio' } })
   }, [address])
 
   return <></>
--- src/pages/[address]/nfts/index.tsx
+++ src/pages/[address]/nfts/index.tsx
@@ -2,13 +2,14 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 const NftsPage = () => {
-  const { query, push } = useRouter()
+  const { query, replace } = useRouter()
 
   const address = query.address as string
 
   useEffect(() => {
     if(!address) return
-    push({ pathname: '/[address]', query: { ...query, tab: 'nfts' }})
+    
+    replace({ pathname: '/[address]', query: { ...query, tab: 'nfts' }})
   }, [address])
 
   return <></>
