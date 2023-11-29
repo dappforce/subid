9b4e19b01bcc4a4d224d613e6bda55297cf2f08e
Mell <oleh.melnechyk@gmail.com>
2023-11-29 11:27:55 +0200

    Fix styling

--- src/components/transfer/configs/cross-chain.ts
+++ src/components/transfer/configs/cross-chain.ts
@@ -4,10 +4,10 @@ import { AltairAdapter } from '@polkawallet/bridge/adapters/centrifuge'
 import { ShadowAdapter } from '@polkawallet/bridge/adapters/crust'
 import { CrabAdapter } from '@polkawallet/bridge/adapters/darwinia'
 import {
-    BasiliskAdapter,
-    HydraAdapter,
-    hydraRouteConfigs,
-    hydraTokensConfig
+  BasiliskAdapter,
+  HydraAdapter,
+  hydraRouteConfigs,
+  hydraTokensConfig,
 } from '@polkawallet/bridge/adapters/hydradx'
 import { InterlayAdapter, KintsugiAdapter } from '@polkawallet/bridge/adapters/interlay'
 import { KicoAdapter } from '@polkawallet/bridge/adapters/kico'
@@ -28,15 +28,15 @@ import { SubsocialAdapter, subsocialTokensConfig } from './custom/SubsocialAdapt
 
 // Add SUB for Hydra DX
 hydraRouteConfigs.push({
-    from: 'hydradx',
-    to: 'subsocial' as ChainId,
-    token: 'SUB',
-    xcm: {
-        fee: {
-            token: 'SUB',
-            amount: '225000000' // TODO: need to be updated
-        },
-    }
+  from: 'hydradx',
+  to: 'subsocial' as ChainId,
+  token: 'SUB',
+  xcm: {
+    fee: {
+      token: 'SUB',
+      amount: '225000000', // TODO: need to be updated
+    },
+  },
 })
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
@@ -46,140 +46,140 @@ hydraTokensConfig.SUB = subsocialTokensConfig.SUB
 console.log(hydraRouteConfigs, hydraTokensConfig)
 
 const availableAdapters: Record<string, { adapter: BaseCrossChainAdapter; chainName?: ChainId }> = {
-    polkadot: {
-        adapter: new PolkadotAdapter(),
-    },
-    kusama: {
-        adapter: new KusamaAdapter(),
-    },
-    karura: {
-        adapter: new KaruraAdapter(),
-    },
-    astar: {
-        adapter: new AstarAdapter(),
-    },
-    shiden: {
-        adapter: new ShidenAdapter(),
-    },
-    acala: {
-        adapter: new AcalaAdapter(),
-    },
-    statemine: {
-        adapter: new StatemineAdapter(),
-    },
-    statemint: {
-        adapter: new StatemineAdapter(),
-    },
-    altair: {
-        adapter: new AltairAdapter(),
-    },
-    shadow: {
-        adapter: new ShadowAdapter(),
-    },
-    'darwinia-crab-parachain': {
-        adapter: new CrabAdapter(),
-        chainName: 'crab'
-    },
-    basilisk: {
-        adapter: new BasiliskAdapter(),
-    },
-    kintsugi: {
-        adapter: new KintsugiAdapter(),
-    },
-    interlay: {
-        adapter: new InterlayAdapter(),
-    },
-    kico: {
-        adapter: new KicoAdapter(),
-    },
-    pichiu: {
-        adapter: new PichiuAdapter(),
-    },
-    calamari: {
-        adapter: new CalamariAdapter(),
-    },
-    moonbeam: {
-        adapter: new MoonbeamAdapter(),
-    },
-    moonriver: {
-        adapter: new MoonriverAdapter(),
-    },
-    khala: {
-        adapter: new KhalaAdapter(),
-    },
-    bifrostKusama: {
-        adapter: new BifrostAdapter(),
-        chainName: 'bifrost'
-    },
-    integritee: {
-        adapter: new IntegriteeAdapter(),
-    },
-    turing: {
-        adapter: new TuringAdapter(),
-    },
-    parallel: {
-        adapter: new ParallelAdapter(),
-    },
-    heiko: {
-        adapter: new HeikoAdapter(),
-        chainName: 'heiko'
-    },
-    hydradx: {
-        adapter: new HydraAdapter(),
-    },
-    zeitgeist: {
-        adapter: new ZeitgeistAdapter(),
-    },
-    subsocial: {
-        adapter: new SubsocialAdapter(),
-    },
+  polkadot: {
+    adapter: new PolkadotAdapter(),
+  },
+  kusama: {
+    adapter: new KusamaAdapter(),
+  },
+  karura: {
+    adapter: new KaruraAdapter(),
+  },
+  astar: {
+    adapter: new AstarAdapter(),
+  },
+  shiden: {
+    adapter: new ShidenAdapter(),
+  },
+  acala: {
+    adapter: new AcalaAdapter(),
+  },
+  statemine: {
+    adapter: new StatemineAdapter(),
+  },
+  statemint: {
+    adapter: new StatemineAdapter(),
+  },
+  altair: {
+    adapter: new AltairAdapter(),
+  },
+  shadow: {
+    adapter: new ShadowAdapter(),
+  },
+  'darwinia-crab-parachain': {
+    adapter: new CrabAdapter(),
+    chainName: 'crab',
+  },
+  basilisk: {
+    adapter: new BasiliskAdapter(),
+  },
+  kintsugi: {
+    adapter: new KintsugiAdapter(),
+  },
+  interlay: {
+    adapter: new InterlayAdapter(),
+  },
+  kico: {
+    adapter: new KicoAdapter(),
+  },
+  pichiu: {
+    adapter: new PichiuAdapter(),
+  },
+  calamari: {
+    adapter: new CalamariAdapter(),
+  },
+  moonbeam: {
+    adapter: new MoonbeamAdapter(),
+  },
+  moonriver: {
+    adapter: new MoonriverAdapter(),
+  },
+  khala: {
+    adapter: new KhalaAdapter(),
+  },
+  bifrostKusama: {
+    adapter: new BifrostAdapter(),
+    chainName: 'bifrost',
+  },
+  integritee: {
+    adapter: new IntegriteeAdapter(),
+  },
+  turing: {
+    adapter: new TuringAdapter(),
+  },
+  parallel: {
+    adapter: new ParallelAdapter(),
+  },
+  heiko: {
+    adapter: new HeikoAdapter(),
+    chainName: 'heiko',
+  },
+  hydradx: {
+    adapter: new HydraAdapter(),
+  },
+  zeitgeist: {
+    adapter: new ZeitgeistAdapter(),
+  },
+  subsocial: {
+    adapter: new SubsocialAdapter(),
+  },
 }
 
 function getPolkawalletChainName (chain: string) {
-    const chainData = availableAdapters[chain]
+  const chainData = availableAdapters[chain]
 
-    if (!chainData) return undefined
-    return chainData.chainName || chain as ChainId
+  if (!chainData) return undefined
+  return chainData.chainName || chain as ChainId
 }
 
 const polkawalletChainToSubidNetworkMap = Object.entries(
-    availableAdapters
+  availableAdapters,
 ).reduce((acc, [ subIdNetwork, { chainName } ]) => {
-    acc[chainName || subIdNetwork as ChainId] = subIdNetwork
-    return acc
+  acc[chainName || subIdNetwork as ChainId] = subIdNetwork
+  return acc
 }, {} as Record<ChainId, string>)
 
 const bridge = new Bridge({ adapters: Object.values(availableAdapters).map(({ adapter }) => adapter) })
 
 export function getCrossChainBridge () {
-    return bridge
+  return bridge
 }
 
 export function getCrossChainAdapter (chain: string): BaseCrossChainAdapter | undefined {
-    return availableAdapters[chain]?.adapter
+  return availableAdapters[chain]?.adapter
 }
 
 export function isTokenBridgeable (token: string) {
-    const sourceChains = bridge.router.getFromChains({ token })
-    const destChains = bridge.router.getDestinationChains({ token })
+  const sourceChains = bridge.router.getFromChains({ token })
+  const destChains = bridge.router.getDestinationChains({ token })
 
-    const filterAvailableChains = (chain: Chain) => {
-        const networkName = polkawalletChainToSubidNetworkMap[chain.id as ChainId]
-        return availableAdapters[networkName]
-    }
+  const filterAvailableChains = (chain: Chain) => {
+    const networkName = polkawalletChainToSubidNetworkMap[chain.id as ChainId]
+    return availableAdapters[networkName]
+  }
 
-    const filteredSourceChains = sourceChains.filter(filterAvailableChains)
-    const filteredDestChains = destChains.filter(filterAvailableChains)
+  const filteredSourceChains = sourceChains.filter(filterAvailableChains)
+  const filteredDestChains = destChains.filter(filterAvailableChains)
 
-    const hasSourceChains = filteredSourceChains.length > 0
-    const hasDestChains = filteredDestChains.length > 0
+  const hasSourceChains = filteredSourceChains.length > 0
+  const hasDestChains = filteredDestChains.length > 0
 
-    let isSameChains = false
-    if (filteredSourceChains.length === 1 && filteredDestChains.length === 1) {
-        isSameChains = filteredSourceChains[0].id === filteredDestChains[0].id
-    }
+  let isSameChains = false
+  if (filteredSourceChains.length === 1 && filteredDestChains.length === 1) {
+    isSameChains = filteredSourceChains[0].id === filteredDestChains[0].id
+  }
 
-    return hasSourceChains && hasDestChains && !isSameChains
+  return hasSourceChains && hasDestChains && !isSameChains
 }
 
 /**
@@ -191,39 +191,39 @@ export function isTokenBridgeable (token: string) {
  * @returns new object that doesn't include falsy properties
  */
 function deleteUndefinedAttributes<T extends object> (obj: T) {
-    return Object.entries(obj).reduce<T>((acc, [ key, value ]) => {
-        if (value) {
-            acc[key as keyof T] = value
-        }
-        return acc
-    }, {} as T)
+  return Object.entries(obj).reduce<T>((acc, [ key, value ]) => {
+    if (value) {
+      acc[key as keyof T] = value
+    }
+    return acc
+  }, {} as T)
 }
 
 type AugmentedRouterFilter = {
-    from?: string
-    to?: string
-    token?: string
+  from?: string
+  to?: string
+  token?: string
 }
 
 export function getRouteOptions (type: 'dest', params: Omit<AugmentedRouterFilter, 'to'>): string[]
 export function getRouteOptions (type: 'source', params: Omit<AugmentedRouterFilter, 'from'>): string[]
 export function getRouteOptions (type: 'source' | 'dest', params: AugmentedRouterFilter): string[] {
-    const bridge = getCrossChainBridge()
-    let options: Chain[] = []
-    try {
-        const parsedParams: RouterFilter = deleteUndefinedAttributes({
-            token: params.token,
-            from: getPolkawalletChainName(params.from ?? ''),
-            to: getPolkawalletChainName(params.to ?? '')
-        })
-        if (type === 'source') {
-            options = bridge.router.getFromChains(parsedParams)
-        } else {
-            options = bridge.router.getDestinationChains(parsedParams)
-        }
-    } catch {
-        options = []
+  const bridge = getCrossChainBridge()
+  let options: Chain[] = []
+  try {
+    const parsedParams: RouterFilter = deleteUndefinedAttributes({
+      token: params.token,
+      from: getPolkawalletChainName(params.from ?? ''),
+      to: getPolkawalletChainName(params.to ?? ''),
+    })
+    if (type === 'source') {
+      options = bridge.router.getFromChains(parsedParams)
+    } else {
+      options = bridge.router.getDestinationChains(parsedParams)
     }
-    return options.map(({ id }) => polkawalletChainToSubidNetworkMap[id as ChainId])
+  } catch {
+    options = []
+  }
+  return options.map(({ id }) => polkawalletChainToSubidNetworkMap[id as ChainId])
 }
 
