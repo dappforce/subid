46c608baf269cb5051e30c7e5ff640164185aab0
Sam <sam4uk.v1ad@gmail.com>
2024-03-12 12:50:23 +0200

    Add redirect for content staking page
    - update subsocial explorer link

--- src/pages/creators/index.tsx
+++ src/pages/creators/index.tsx
@@ -1,22 +1,11 @@
-import CreatorsStakingPage from 'src/components/main/CreatorStakingPage'
-import { getInitialPropsWithRedux } from 'src/rtk/app/nextHelpers'
-import { fetchCreatorsList } from '../../rtk/features/creatorStaking/creatorsList/creatorsListHooks'
-import { FETCH_CHIAN_INFO_WITH_PRICES } from 'src/rtk/app/actions'
-import { fetchStakingConsts } from '../../rtk/features/creatorStaking/stakingConsts/stakingConstsHooks'
+import { redirect } from 'next/navigation'
 
-getInitialPropsWithRedux(CreatorsStakingPage, async ({ dispatch }) => {
-  dispatch({ type: FETCH_CHIAN_INFO_WITH_PRICES })
-  fetchCreatorsList(dispatch)
-  fetchStakingConsts(dispatch)
+const CreatorsStakingPage = () => {
+  return <></>
+}
 
-  return {
-    head: {
-      title: 'Join The Creator Economy On Subsocial',
-      desc: 'Earn rewards when you post content or engage with others in Subsocial\'s Content Staking system. Lock some SUB to get started today!',
-      image: '/images/creator-staking/meta-bg.jpg',
-      forceTitle: true,
-    },
-  }
-})
+export const getServerSideProps = () => {
+  redirect('https://grillapp.net/c/staking')
+}
 
 export default CreatorsStakingPage
